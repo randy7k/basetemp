70103fab4138459167848418815ca84cff41c25b
randy7k <randyr7k@gmail.com>
2017-10-11 08:08:20 -0400

    edit the singup template and add names fildes

--- account/views.py
+++ account/views.py
@@ -1,32 +1,26 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
 from django import forms
-from .forms import UserRegistrationForm
+from .forms import SignUpForm
+
 # from __future__ import unicode_literals
 
 def home(request):
     return render(request, 'account/home.html')
 
 def signup(request):
-    user_exists_error = ""
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            userObj = form.cleaned_data
-            username = userObj['username']
-            email =  userObj['email']
-            password =  userObj['password']
-            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
-                User.objects.create_user(username, email, password)
-                user = authenticate(username = username, password = password)
-                login(request, user)
-                return HttpResponseRedirect('/')
-            else:
-                user_exists_error = "Looks like a username with that email or password already exists"
-                # raise forms.ValidationError('Looks like a username with that email or password already exists')
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
     else:
-        form = UserRegistrationForm()
-
-    return render(request, 'account/signup.html', {'form' : form, 'user_exists_error' : user_exists_error})
+        form = SignUpForm()
+    return render(request, 'account/signup.html', {'form': form})
--- basetemp/settings.py
+++ basetemp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l^iy2$(td-ftl%7$-tck&k^dg!+5(l*dki9jf=^zu!&g!glykt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ]
 
 LOGIN_REDIRECT_URL = '/'
 
